f4daf97ddee305d78acd8c2a88a4e73ecd86d63d
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-03-23 10:51:31 -0700

    Advanced Tasks

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	long int n;
 	long int max;
+	long int i;
 
 	n = 612852475143;
 	max = -1;
@@ -20,7 +21,7 @@ int main(void)
 		n /= 2;
 	}
 
-	for (long int i = 3; i <= sqrt(n); i += 2)
+	for (i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
 		{
