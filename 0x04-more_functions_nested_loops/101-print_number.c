5e899e5b73724b04c62cd26afffa8882b510378d
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-03-23 10:42:41 -0700

    Mandatory Tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	int number2;
+
+	if (n < 0)
+	{
+		number2 = -n;
+		_putchar('-');
+	}
+	else
+		number2 = n;
+
+	if (number2 / 10)
+		print_number(number2 / 10);
+
+	_putchar((number2 % 10));
+}
