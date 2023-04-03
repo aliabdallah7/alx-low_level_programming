4d26626f87398f8059ed19dea107ae47ee90fbf1
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-03 05:30:12 -0700

    Third Task for the 100 time

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] > '\0')
+	while (*s > '\0')
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (&s[i]);
+			return (s);
 		}
 		i++;
 	}
