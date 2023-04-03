eb55ec3ea2f99c7b1df2ba7c3200aaac5fb4e6e7
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-03 14:20:29 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,10 +10,10 @@ void print_chessboard(char (*a)[8])
 	char *p = &a[0][0];
 	int i;
 
-	for (i = 0; i < 8; i++, p++)
+	for (i = 0; i < 64; i++, p++)
 	{
 		_putchar(*p);
 	}
-	_putchar('\0');
+	_putchar('\n');
 }
 
