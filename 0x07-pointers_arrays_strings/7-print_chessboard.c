e7b3bcbbdeeab79da87a5229c4107e384a71c56d
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-03 14:54:44 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,19 +3,16 @@
 /**
  * print_chessboard - Entry point
  * @a: 2d_array
- * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	char *p = &a[0][0];
 	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(*p);
-			p++;
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
