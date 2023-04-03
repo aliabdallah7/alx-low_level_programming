18e9c4dbf0d843ecc9957cd8b4d598755789641f
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-03 06:22:16 -0700

    Fourth Task for the 8th time

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,11 +10,11 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count = 0;
 	int counterLoop;
 
-	for (int i = 0; s[i] >= '\0'; i++)
+	for (; *s; s++)
 	{
 		for (counterLoop = 0; accept[counterLoop]; counterLoop++)
 		{
-			if (s[i] == accept[counterLoop])
+			if (*s == accept[counterLoop])
 			{
 				counterLoop++;
 				break;
