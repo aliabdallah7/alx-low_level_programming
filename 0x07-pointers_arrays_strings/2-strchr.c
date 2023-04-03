e100a9933f2e31edcf6dc0ba0b65de2748b33b83
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-03 05:18:38 -0700

    Third Task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,19 +8,18 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
+	for (int i = 0; s[i] != '\0'; i++)
 	{
-            return (s);
-        }
-        s++;
-    }
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
 
-    if (c == '\0')
-    {
-        return (s);
-    }
+	if (c == '\0')
+	{
+		return (&s[strlen(s)]);
+	}
 
-    return (NULL);
+	return (NULL);
 }
