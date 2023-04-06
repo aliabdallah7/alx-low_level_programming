fbb9f88773f8d04bce9682370053f5af7d09c969
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-05 17:17:06 -0700

    Handling betty

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,20 +6,24 @@ int is_palindrome_helper(char *s, int start, int end);
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
- * @i: iterator
  * Return: 1 if it is, 0 it's not
  */
 
 int is_palindrome(char *s)
 {
 	int length = strlen(s);
-	return is_palindrome_helper(s, 0, length-1);
+
+	return (is_palindrome_helper(s, 0, length - 1));
 }
 
-/*
- * is_palindrome_helper - compares the characters at the start and end.
+/**
+ * is_palindrome_helper - compares the characters at the start and end
+ * @s: string to reserve
+ * @start: start of palindrome
+ * @end: end of palindrome
+ *
+ * Return: itself with start + 1 and end - 1
  */
-
 int is_palindrome_helper(char *s, int start, int end)
 {
 	if (start >= end)
@@ -32,6 +36,6 @@ int is_palindrome_helper(char *s, int start, int end)
 	}
 	else
 	{
-		return is_palindrome_helper(s, start+1, end-1);
+		return (is_palindrome_helper(s, start + 1, end - 1));
 	}
 }
