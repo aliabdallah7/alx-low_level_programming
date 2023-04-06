27b7dd1132cf703cdf92de40c24ab1c4f4003484
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-05 17:52:13 -0700

    Handling the loop

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,26 @@
 #include "main.h"
-
+int is_prime_helper(int n, int i);
 /**
- * is_prime_number - says if an integer is a prime number or not
+ * is_prime_helper - recursive helper function
+ * to determine if a number is prime
  * @n: number to evaluate
- * @i: iterator
+ * @i: divisor to check
  * Return: 1 if n is a prime number, 0 if not
  */
-
-int is_prime_number(int n)
+int is_prime_helper(int n, int i)
 {
-	int i;
-
-	if (n < 2)
-	{
+	if ((n < 2) || (n % 2 == 0) || (n % i == 0))
 		return (0);
-	}
-	for (i = 2; i <= n/2; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-
-	return (1);
+	else if (n == 2 || i > n / 2)
+		return (1);
+	else
+		return (is_prime_helper(n, i + 2));
 }
+
+/**
+ * is_prime_number - determines if a number is prime using recursion
+ * @n: number to evaluate
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_number(int n)
+{ return (is_prime_helper(n, 3)); }
