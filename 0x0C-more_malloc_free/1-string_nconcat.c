5be1f9132da4440fb751c076a1c831cc57d214c9
aliabdallah7 <ali.abdallah43792@gmail.com>
2023-04-13 12:41:36 -0700

    Mandatory Tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,17 +15,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size_t s2_len;
 	char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-        if (s2 == NULL)
-		s2 = "";
-
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	if (n >= s2_len)
 		n = s2_len;
 
 	result = malloc(s1_len + n + 1);
+
 	if (result == NULL)
 		return (NULL);
 
